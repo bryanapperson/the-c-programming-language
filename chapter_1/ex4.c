d28d0a9cdfde315d703711cfc8a0ea5d30796a36
Bryan Apperson <me@bryanapperson.com>
2016-12-04 22:00:31 -0500

    fix ex4

--- chapter_1/ex4.c
+++ chapter_1/ex4.c
@@ -14,14 +14,14 @@ int main(int argc, char *argv[]) {
 
     ratio = 9.0 / 5.0;
 
-    celcius = lower;
+    celsius = lower;
 
     printf("This is a simple program that converts\n");
     printf("Fahrenheit to Celsius in decimal format\n");
 
     while (celsius <= upper) {
-        fahr = celcius * ratio + 32;
-        printf("Fahrenheit: %6.2f Celsius: %3.1f\n", fahr, celcius);
+        fahr = celsius * ratio + 32;
+        printf("Fahrenheit: %6.2f Celsius: %3.1f\n", fahr, celsius);
         celsius = celsius + step;
     }
     return 0;
