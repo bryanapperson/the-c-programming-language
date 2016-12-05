5fe2bbba0ba19f8e58e1a814dfbfa10a980d2254
Bryan Apperson <me@bryanapperson.com>
2016-12-04 22:31:36 -0500

    add simple wc implementation for testing ex11

---
+++ chapter_1/wc.c
@@ -0,0 +1,33 @@
+/*
+ * Created by Bryan Apperson on 12/4/16.
+ *
+ * Word count program form section 1.5.4 of K&R C for testing modified to
+ * return a value to the OS.
+ * */
+
+#include <stdio.h>
+
+#define IN 1 /* inside a word */
+#define OUT 0 /* outside a word */
+
+int main() {
+    /* count lines, words, and characters in input */
+    int c, nl, nw, nc, state;
+    state = OUT;
+    nl = nw = nc = 0;
+    while ((c = getchar()) != EOF) {
+        ++nc;
+        if (c == ' ' || c == '\n' || c == '\t') {
+            state = OUT;
+            if (c == '\n') {
+                ++nl;
+            }
+        } else if (state == OUT) {
+            state = IN;
+            ++nw;
+        }
+    }
+    // All done.
+    return 0;
+}
+
