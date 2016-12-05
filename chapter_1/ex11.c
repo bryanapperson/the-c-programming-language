c512e921c040a8a315ea11b1a1b501ee6940be59
Bryan Apperson <me@bryanapperson.com>
2016-12-04 22:17:44 -0500

    add skeleton for chapter 11

---
+++ chapter_1/ex11.c
@@ -0,0 +1,14 @@
+//
+// Created by Bryan Apperson on 12/4/16.
+//
+/* This program should test the wc (word count) program from K&R C section
+ * 1.5.4
+ *
+ * The test cases are as follows:
+ * */
+
+int main() {
+    // Execute test cases and return success or failure.
+    return 0;
+}
+
