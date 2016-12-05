36377136e3dc92067cac4a357f50cb4db3110412
Bryan Apperson <me@bryanapperson.com>
2016-12-04 22:09:49 -0500

    change wording

--- chapter_1/ex2.c
+++ chapter_1/ex2.c
@@ -7,7 +7,7 @@ int main() {
     printf("Audible or visual alert. \a\n");
     printf("Form feed. \f\n");
     printf("This escape, \r, moves the active position to the initial position of the current line.\n");
-    printf("Vertical tab \v is tricky, as its behaviour is unspecified under certain conditions.\n");
+    printf("Vertical tab \v is strange, as its behaviour is unspecified in certain conditions.\n");
 
     return 0;
 }
